02f8af0a9afca5ec69ad24a4410be2a129dab567
ivanfsilva <ivanfs27@gmail.com>
2021-06-12 15:19:39 -0300

    Adding success message with PrimeNG Toasty

--- front-angular/src/app/app.module.ts
+++ front-angular/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+
 import { AppComponent } from './app.component';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { CoreModule } from './core/core.module';
 import { HttpClientModule } from '@angular/common/http';
-import {LancamentoService} from './lancamentos/lancamento.service';
+import { LancamentoService } from './lancamentos/lancamento.service';
+
 
 @NgModule({
   declarations: [
@@ -20,11 +24,13 @@ import {LancamentoService} from './lancamentos/lancamento.service';
     FormsModule,
     HttpClientModule,
 
+    ToastModule,
+
     CoreModule,
     LancamentosModule,
     PessoasModule
   ],
-  providers: [LancamentoService],
+  providers: [LancamentoService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
