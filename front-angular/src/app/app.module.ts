b76d38ed2eb22b82d7ef40ad2b5814942d34ae34
ivanfsilva <ivanfs27@gmail.com>
2021-06-11 23:30:24 -0300

    Creating the Financial query Service

--- front-angular/src/app/app.module.ts
+++ front-angular/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { CoreModule } from './core/core.module';
+import { HttpClientModule } from '@angular/common/http';
+import {LancamentoService} from './lancamentos/lancamento.service';
 
 @NgModule({
   declarations: [
@@ -16,12 +18,13 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    HttpClientModule,
 
     CoreModule,
     LancamentosModule,
     PessoasModule
   ],
-  providers: [],
+  providers: [LancamentoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
