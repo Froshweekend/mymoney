8a8102b2b6f4c1a46542537271034137a665c817
ivanfsilva <ivanfs27@gmail.com>
2021-06-17 11:37:17 -0300

    fixing bug in the security of the application "cannot cast UsuarioSistema"

--- backend/src/main/java/br/com/ivanfsilva/mymoneyapi/config/AuthorizationServerConfig.java
+++ backend/src/main/java/br/com/ivanfsilva/mymoneyapi/config/AuthorizationServerConfig.java
@@ -1,24 +1,17 @@
 package br.com.ivanfsilva.mymoneyapi.config;
 
-import br.com.ivanfsilva.mymoneyapi.config.token.CustomTokenEnhancer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenEnhancer;
-import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
-import java.util.Arrays;
-
-@Profile("oauth-security")
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
@@ -35,7 +28,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .authorizedGrantTypes("password", "refresh_token")
                 .accessTokenValiditySeconds(1800)
                 .refreshTokenValiditySeconds(3600 * 24)
-            .and()
+                .and()
                 .withClient("mobile")
                 .secret("m0b1l30")
                 .scopes("read")
@@ -46,12 +39,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
-        tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
-
         endpoints
                 .tokenStore(tokenStore())
-                .tokenEnhancer(tokenEnhancerChain)
+                .accessTokenConverter(accessTokenConverter())
                 .reuseRefreshTokens(false)
                 .authenticationManager(authenticationManager);
     }
@@ -66,10 +56,5 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Bean
     public TokenStore tokenStore() {
         return new JwtTokenStore(accessTokenConverter());
-    }
-
-    @Bean
-    public TokenEnhancer tokenEnhancer() {
-        return new CustomTokenEnhancer();
     }
 }
--- backend/src/main/java/br/com/ivanfsilva/mymoneyapi/config/token/CustomTokenEnhancer.java
+++ backend/src/main/java/br/com/ivanfsilva/mymoneyapi/config/token/CustomTokenEnhancer.java
@@ -1,13 +1,14 @@
 package br.com.ivanfsilva.mymoneyapi.config.token;
 
-import br.com.ivanfsilva.mymoneyapi.security.UsuarioSistema;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 
-import java.util.HashMap;
-import java.util.Map;
+import br.com.ivanfsilva.mymoneyapi.security.UsuarioSistema;
 
 public class CustomTokenEnhancer implements TokenEnhancer {
 
