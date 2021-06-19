8ab048d43241360cbc6ecf32e77d1ea8cbfc4462
ivanfsilva <ivanfs27@gmail.com>
2021-06-19 11:39:49 -0300

    Decoding the JWT and storing it in Local Storage

--- front-angular/src/app/seguranca/seguranca.module.ts
+++ front-angular/src/app/seguranca/seguranca.module.ts
@@ -5,6 +5,8 @@ import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 
+import { JwtModule, JwtHelperService } from '@auth0/angular-jwt';
+
 import { SegurancaRoutingModule } from './seguranca-routing.module';
 import { LoginFormComponent } from './login-form/login-form.component';
 
@@ -16,8 +18,17 @@ import { LoginFormComponent } from './login-form/login-form.component';
     InputTextModule,
     ButtonModule,
 
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: () => {
+          return '';
+        }
+      }
+    }),
+
     SegurancaRoutingModule
   ],
+  providers: [JwtHelperService],
   declarations: [LoginFormComponent]
 })
 export class SegurancaModule { }
