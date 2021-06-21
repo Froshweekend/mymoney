22934425f7691bc25c36c78152390ddcca4aec92
ivanfsilva <ivanfs27@gmail.com>
2021-06-21 18:59:29 -0300

    Searching Expired Expenses and Income with Spring Data JPA

--- backend/src/main/java/br/com/ivanfsilva/mymoneyapi/repository/LancamentoRepository.java
+++ backend/src/main/java/br/com/ivanfsilva/mymoneyapi/repository/LancamentoRepository.java
@@ -1,5 +1,8 @@
 package br.com.ivanfsilva.mymoneyapi.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import br.com.ivanfsilva.mymoneyapi.model.Lancamento;
 import br.com.ivanfsilva.mymoneyapi.repository.lancamento.LancamentoRepositoryQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +11,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long>, LancamentoRepositoryQuery {
 
+    List<Lancamento> findByDataVencimentoLessThanEqualAndDataPagamentoIsNull(LocalDate data);
 }
--- backend/src/main/java/br/com/ivanfsilva/mymoneyapi/repository/UsuarioRepository.java
+++ backend/src/main/java/br/com/ivanfsilva/mymoneyapi/repository/UsuarioRepository.java
@@ -3,10 +3,13 @@ package br.com.ivanfsilva.mymoneyapi.repository;
 import br.com.ivanfsilva.mymoneyapi.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UsuarioRepository  extends JpaRepository<Usuario, Long> {
 
     public Optional<Usuario> findByEmail(String email);
+
+    public List<Usuario> findByPermissoesDescricao(String permissaoDescricao);
 
 }
