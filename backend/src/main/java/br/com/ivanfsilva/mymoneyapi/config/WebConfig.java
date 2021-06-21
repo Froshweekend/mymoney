f7e587fb9276a61513d109d74493495de634651e
ivanfsilva <ivanfs27@gmail.com>
2021-06-21 16:37:21 -0300

    Creating a Task Schedule (Scheduler)

---
+++ backend/src/main/java/br/com/ivanfsilva/mymoneyapi/config/WebConfig.java
@@ -0,0 +1,9 @@
+package br.com.ivanfsilva.mymoneyapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class WebConfig {
+}
