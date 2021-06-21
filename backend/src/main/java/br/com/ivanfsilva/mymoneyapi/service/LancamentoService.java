975b57fabf2693aff92200c18f02d9933d0aa872
ivanfsilva <ivanfs27@gmail.com>
2021-06-21 20:22:42 -0300

    Including logs in the email sending schedule

--- backend/src/main/java/br/com/ivanfsilva/mymoneyapi/service/LancamentoService.java
+++ backend/src/main/java/br/com/ivanfsilva/mymoneyapi/service/LancamentoService.java
@@ -1,5 +1,8 @@
 package br.com.ivanfsilva.mymoneyapi.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import br.com.ivanfsilva.mymoneyapi.dto.LancamentoEstatisticaPessoa;
 import br.com.ivanfsilva.mymoneyapi.mail.Mailer;
 import br.com.ivanfsilva.mymoneyapi.model.Lancamento;
@@ -31,6 +34,8 @@ public class LancamentoService {
 
     private static final String DESTINATARIOS = "ROLE_PESQUISAR_LANCAMENTO";
 
+    private static final Logger logger = LoggerFactory.getLogger(LancamentoService.class);
+
     @Autowired
     private PessoaRepository pessoaRepository;
 
@@ -45,14 +50,35 @@ public class LancamentoService {
 
     @Scheduled(cron = "0 0 6 * * *")
     public void avisarSobreLancamentosVencidos() {
+        if (logger.isDebugEnabled()) {
+            logger.debug("Preparando envio de "
+                    + "e-mails de aviso de lançamentos vencidos.");
+        }
 
         List<Lancamento> vencidos = lancamentoRepository
                 .findByDataVencimentoLessThanEqualAndDataPagamentoIsNull(LocalDate.now());
 
+        if (vencidos.isEmpty()) {
+            logger.info("Sem lançamentos vencidos para aviso.");
+
+            return;
+        }
+
+        logger.info("Exitem {} lançamentos vencidos.", vencidos.size());
+
         List<Usuario> destinatarios = usuarioRepository
                 .findByPermissoesDescricao(DESTINATARIOS);
 
+        if (destinatarios.isEmpty()) {
+            logger.warn("Existem lançamentos vencidos, mas o "
+                    + "sistema não encontrou destinatários.");
+
+            return;
+        }
+
         mailer.avisarSobreLancamentosVencidos(vencidos, destinatarios);
+
+        logger.info("Envio de e-mail de aviso concluído.");
     }
 
     public byte[] relatorioPorPessoa(LocalDate inicio, LocalDate fim) throws Exception {
