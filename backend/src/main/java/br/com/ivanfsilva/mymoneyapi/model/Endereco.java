915a182fd4024a3b325dae2722fb9306af89d89d
ivanfsilva <ivanfs27@gmail.com>
2021-06-25 10:21:02 -0300

    Creating city and state entities

--- backend/src/main/java/br/com/ivanfsilva/mymoneyapi/model/Endereco.java
+++ backend/src/main/java/br/com/ivanfsilva/mymoneyapi/model/Endereco.java
@@ -1,38 +1,28 @@
 package br.com.ivanfsilva.mymoneyapi.model;
 
 import javax.persistence.Embeddable;
-import javax.validation.constraints.Size;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Embeddable
 public class Endereco {
 
-    @Size(max = 50)
-    private String Logradouro;
-
-    @Size(max = 30)
+    private String logradouro;
     private String numero;
-
-    @Size(max = 30)
     private String complemento;
-
-    @Size(max = 30)
     private String bairro;
-
-    @Size(max = 30)
-    private String cidade;
-
-    @Size(max = 30)
     private String cep;
 
-    @Size(max = 30)
-    private String estado;
+    @ManyToOne
+    @JoinColumn(name = "codigo_cidade")
+    private Cidade cidade;
 
     public String getLogradouro() {
-        return Logradouro;
+        return logradouro;
     }
 
     public void setLogradouro(String logradouro) {
-        Logradouro = logradouro;
+        this.logradouro = logradouro;
     }
 
     public String getNumero() {
@@ -59,14 +49,6 @@ public class Endereco {
         this.bairro = bairro;
     }
 
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
     public String getCep() {
         return cep;
     }
@@ -75,11 +57,11 @@ public class Endereco {
         this.cep = cep;
     }
 
-    public String getEstado() {
-        return estado;
+    public Cidade getCidade() {
+        return cidade;
     }
 
-    public void setEstado(String estado) {
-        this.estado = estado;
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
     }
 }
