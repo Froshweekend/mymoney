1ca7a3e3c5ce94e30e3e9e9833a31f9428e85a74
ivanfsilva <ivanfs27@gmail.com>
2021-06-25 10:49:08 -0300

    Searching for states and cities

--- front-angular/src/app/pessoas/pessoa.service.ts
+++ front-angular/src/app/pessoas/pessoa.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 
 import { environment } from './../../environments/environment';
 
-import { Pessoa } from './../core/model';
+import { Cidade, Estado, Pessoa } from './../core/model';
 
 export class PessoaFiltro {
   nome: string;
@@ -15,13 +15,16 @@ export class PessoaFiltro {
 export class PessoaService {
 
   pessoasUrl: string;
+  cidadesUrl: string;
+  estadosUrl: string;
 
   constructor(private http: HttpClient) {
     this.pessoasUrl = `${environment.apiUrl}/pessoas`;
+    this.estadosUrl = `${environment.apiUrl}/estados`;
+    this.cidadesUrl = `${environment.apiUrl}/cidades`;
   }
 
   pesquisar(filtro: PessoaFiltro): Promise<any> {
-
     let params = new HttpParams()
       .set('page', filtro.pagina.toString())
       .set('size', filtro.itensPorPagina.toString());
@@ -41,45 +44,50 @@ export class PessoaService {
         };
 
         return resultado;
-      });
+      })
   }
 
   listarTodas(): Promise<any> {
-
     return this.http.get(this.pessoasUrl)
       .toPromise()
       .then(response => response['content']);
   }
 
   excluir(codigo: number): Promise<void> {
-
     return this.http.delete(`${this.pessoasUrl}/${codigo}`)
       .toPromise()
       .then(() => null);
   }
 
   mudarStatus(codigo: number, ativo: boolean): Promise<void> {
-
     return this.http.put(`${this.pessoasUrl}/${codigo}/ativo`, ativo)
       .toPromise()
       .then(() => null);
   }
 
   adicionar(pessoa: Pessoa): Promise<Pessoa> {
-
     return this.http.post<Pessoa>(this.pessoasUrl, pessoa)
       .toPromise();
   }
 
   atualizar(pessoa: Pessoa): Promise<Pessoa> {
-
     return this.http.put<Pessoa>(`${this.pessoasUrl}/${pessoa.codigo}`, pessoa)
       .toPromise();
   }
 
   buscarPorCodigo(codigo: number): Promise<Pessoa> {
-
     return this.http.get<Pessoa>(`${this.pessoasUrl}/${codigo}`)
       .toPromise();
+  }
+
+  listarEstados(): Promise<Estado[]> {
+    return this.http.get<Estado[]>(this.estadosUrl).toPromise();
+  }
+
+  pesquisarCidades(estado): Promise<Cidade[]> {
+    const params = new HttpParams()
+      .set('estado', estado);
+
+    return this.http.get<Cidade[]>(this.cidadesUrl, { params }).toPromise();
   }
 }
